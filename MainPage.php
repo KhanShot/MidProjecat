a67da37ee6969789e9e2c683927ef7c81594c80d
Rymkhanov Ali <rymkhanov.ali@mail.ru>
2018-12-18 02:56:55 +0600

    Final project

---
+++ MainPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Restaurants</title>
+</head>
+<body>
+	<div id="main-block">
+		<div id="block-header"> <?php include "header.html";?></div>
+		<div id="block-content">Content</div>
+		<div id="block-footer"><?php include "footer.html"; ?></div>
+	</div>
+
+</body>
+</html>
