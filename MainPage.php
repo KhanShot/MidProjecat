e4ac52182c2b39d02e8e5ccc0486171e10a79585
Rymkhanov Ali <rymkhanov.ali@mail.ru>
2018-12-21 17:23:36 +0600

    added changes

--- MainPage.php
+++ MainPage.php
@@ -2,21 +2,44 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="script.js"></script>
 	<title>Restaurants</title>
 </head>
 <body>
 	<div id="main-block">
 		<div id="block-header"> <?php include "header.html";?></div>
-		
-
 		<div id="block-content">
-			<div id="slide">slides</div>
-			asdka
+			<figure>
+				<div class="slideshow-container">
+					<div class="mySlides fade">
+						<img class="imags" src="images/slide1.jpg" style="width:100%">
+					</div>
+					
+					<div class="mySlides fade">
+						<img class="imags" src="images/slide2.jpg" style="width:100%">
+					</div>
 
-		</div>
+					<div class="mySlides fade">
+						<img class="imags" src="images/slide3.jpg" style="width:100%">
+					</div>
+
+					<div class="mySlides fade">
+						<img class="imags" src="images/slide4.jpg" style="width:100%">
+					</div>
+
+					<a class="prev" onclick="changeSlide(-1)">&#10094;</a>
+					<a class="next" onclick="changeSlide(1)">&#10095;</a>
+				</div> <!-- slideshowcontainer -->
 
 
-
+				<div style="text-align:center">
+					<span class="dot" onclick="currentSlide(1)"></span>
+					<span class="dot" onclick="currentSlide(2)"></span>
+					<span class="dot" onclick="currentSlide(3)"></span>
+					<span class="dot" onclick="currentSlide(4)"></span>
+				</div>
+			</figure>
+		</div> <!-- block-content -->
 		<div id="block-footer"><?php include "footer.html";?></div>
 	</div>
 
